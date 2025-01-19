ec835f1a16818e5bcd277e6c0196b8b602feb61f
Nipun Sachintha <lvnsachintha@std.appsc.sab.ac.lk>
2025-01-19 17:36:21 +0530

    Cars page

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import OurCars from '../components/Cars/OurCars'
+import Footer from '../components/Footer'
 
 const Cars = () => {
   return (
     <>
       <Navbar/>
+      <OurCars/>
+      <Footer/>
     </>
   )
 }
